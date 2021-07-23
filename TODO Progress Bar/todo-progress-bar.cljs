7a917ebe30857ab5ec51e8cba764a3cd7acb3605
Matt Vogel <mainmoniker@googlemail.com>
2021-07-22 21:49:53 -0400

    feat: progress bar -support block ref'd TODOs

--- TODO Progress Bar/todo-progress-bar.cljs
+++ TODO Progress Bar/todo-progress-bar.cljs
@@ -1,23 +1,9 @@
-(ns progress-bar-v6
+(ns progress-bar-v10
   (:require
    [reagent.core :as r]
    [datascript.core :as d]
    [roam.datascript.reactive :as dr]
    [clojure.pprint :as pp]))
-
-(defn find-db-id 
-  "Finds the database ID given a page title"
-  [pagename]
-  
-  @(dr/q '[:find (pull ?e [:db/id]) .
-                      :in $ ?pagename
-                      :where
-                      [?e :node/title ?pagename]]
-                    pagename)
-  )
-
-(defonce TODO-ID (atom (:db/id (find-db-id "TODO"))))
-(defonce DONE-ID (atom (:db/id (find-db-id "DONE"))))
 
 (defn flatten-block 
   "Flattens a blocks children into a flat list"
@@ -26,56 +12,83 @@
           (conj acc (dissoc block :block/children))
           (:block/children block)))
 
-(defn flatten-refs 
-  "Flattens blocks refs into a flat list"
-  [ block]
-  (->> block
-     (map :block/refs)
-      (flatten )
-    	(map :db/id)
-    	(frequencies))
-  )
-
-(defn find-child-refs 
+(defn find-child-refs
   "Returns all _refs for children blocks given a parent block uid"
   [block-uid]
-  (flatten-refs  (flatten-block []
+  (flatten-block []
              @(dr/q '[:find (pull ?e [:block/refs{:block/children ...}]) .
                       :in $ ?uid
                       :where
                       [?e :block/uid ?uid]]
                     block-uid)))
+
+(defn id-title 
+  "Gets a page's title from its db id"
+  [id]
+  (:node/title @(dr/pull '[:node/title] id))
   )
 
+(defn info-from-id [id]
+  	(or (:node/title @(dr/pull '[:node/title] id))
+      (map
+       id-title 
+       (map 
+       		:db/id
+       		(:block/refs @(dr/pull '[:block/refs] id))
+       ))
+      )
+  )
+
+(defn count-occurrences 
+  "Counts the occurances of a string in a list"
+  [s slist]
+  (->> slist
+       flatten
+       (filter #{s})
+       count))
+
+
+(defn recurse-search
+  "Recursivly search through a block's children for all pages referenced"
+  [block-uid]
+  (->> block-uid
+       (find-child-refs)
+       (map :block/refs)
+       (flatten)
+       (map :db/id)
+       (map  info-from-id)
+       (flatten)))
+
+
 (defn main [{:keys [block-uid]} & args]
-  (let [tasks (r/atom {
-                       :todo (get (find-child-refs block-uid) @TODO-ID 0)
-                       :done (get (find-child-refs block-uid) @DONE-ID 0) 
-                       } )]
-   
-  [:div
-         [:div {:style {:display "flex"
-                             :align-items "center"}
-                     }
-               [:span [:progress {
-                  :id "file"
-                  :name "percent-done"
-                  :value (:done @tasks)
-                  :max (+ (:todo @tasks) (:done @tasks))
-                  :style{
+  (let [tasks (r/atom {;; don't love that I do this search twice
+                       :todo (count-occurrences "TODO" (recurse-search block-uid))
+                       :done (count-occurrences "DONE" (recurse-search block-uid))} )]
+    
+      [:div
+             [:div {:style {:display "flex"
+                                 :align-items "center"}
+                         }
+                   [:span [:progress {
+                      :id "file"
+                      :name "percent-done"
+                      :value (:done @tasks)
+                      :max (+ (:todo @tasks) (:done @tasks))
+                      :style{
 
-                         :margin-left "10px"
-                         :margin-right "10px"
-                         }}]
-                ]
-                [:span [:div  (str (:done @tasks)  "/"
-                                  (+ 
-                                    (:done @tasks)
-                                    (:todo @tasks))
-                                " Done"
-                                )]]
+                             :margin-left "10px"
+                             :margin-right "10px"
+                             }}]
+                    ]
+                    [:span [:div  (str (:done @tasks)  "/"
+                                      (+ 
+                                        (:done @tasks)
+                                        (:todo @tasks))
+                                    " Done"
+                                    )]]
 
-               ]
-       ]
+                   ]
+           ]
   ))
 ;
+
